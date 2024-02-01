7bd9e1ff4a859ba436321f473367a93d3c4fa61d
Erik Manosso <erikmanosso1@gmail.com>
2024-02-01 10:35:04 -0300

    testes utilizando listas de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress"/>
+import EnderecoPage from '../support/page-objects/endereço.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -6,10 +8,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         cy.fixture('perfil').then(dados =>{
             cy.login(dados.usuario,dados.senha)
         })
-        
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+        EnderecoPage.editarEnderecoFaturamento('Tulio', 'Aparecido','Oi','Brasil','Rua Torta','S/N','Cariacica','Espirito Santo', '87542-550','25999885544','tulililio@testes.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
